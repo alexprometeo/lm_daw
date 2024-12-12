236283be3ebb2b64d8aa0ca4aaba9994f00fb3eb
alexprometeo <alexandru.cucuruzeanu@mercanza.es>
2024-12-12 13:03:37 +0100

    añadidas condiciones;

--- calculadoraPromedio/main.js
+++ calculadoraPromedio/main.js
@@ -1,3 +1,10 @@
+// Si el usuario introduce 1, 2 -> OK
+// Si el usuario introduce "" -> Error: no has introducido ningún número.
+// Si el usuario no lo separa por comas: Error: los elementos deben estar separados por comas.
+// si el usuario itroduce los numeros como palabras -> Error: has introducido un elemento que no es un número.
+
+const elementoResultado = document.getElementById("resultado");
+
 function calcularPromedio(numeros) {
     if (!Array.isArray(numeros) || numeros.length === 0) {
         return "Error: El input debe ser un array no vacío.";
@@ -5,8 +12,8 @@ function calcularPromedio(numeros) {
 
     let suma = 0;
     for (let i = 0; i < numeros.length; i++) {
-        if (typeof numeros[i] !== "number") {
-            return `Error: El elemento en la posición ${i} no es un número.`;
+        if (typeof numeros[i] !== "number" || isNaN(numeros[i])) {
+            return `Error: El elemento en la posición ${i+1} no es un número.`;
         }
         suma += numeros[i];
     }
@@ -19,10 +26,23 @@ let calculo = document.getElementById("calcular");
 calculo.onclick = function () {
     //Recuperar el valor del input
     const input = document.getElementById("numeros").value;
-    const numeros = input.split(",").map(num => parseFloat(num.trim()));
-    const resultado = calcularPromedio(numeros);
-
-    // Mostrar el resultado y registrar en consola
-    console.log("Resultado calculado:", resultado);
-    document.getElementById("resultado").innerText = resultado;
+    if (input.length === 0) {
+        elementoResultado.innerText = "¡Oye que no has introducido nada!";
+    } 
+    //else if (input.includes(",") === false) {
+    else if (!input.includes(",")) {
+        elementoResultado.innerText = "¡Oye que es necesario separar los elementos por comas (,)!";
+    }
+    else if (input.includes("-") || input.includes("?") || input.includes(";")) {
+        elementoResultado.innerText = "¡Oye que es necesario separar los elementos por comas (,)!";
+    }
+    else {
+        const numeros = input.split(",");
+        const decimales = numeros.map(num => parseFloat(num.trim()));
+        const resultado = calcularPromedio(decimales);
+    
+        // Mostrar el resultado y registrar en consola
+        console.log("Resultado calculado:", resultado);
+        elementoResultado.innerText = resultado;
+    }
 }
